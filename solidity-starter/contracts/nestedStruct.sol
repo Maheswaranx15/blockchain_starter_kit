cb027312233fcc5add4ac0bf57a6fa11a83a2110
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-04-07 10:00:19 +0530

    nested structs contracts added

--- solidity-starter/contracts/nestedStruct.sol
+++ solidity-starter/contracts/nestedStruct.sol
@@ -6,7 +6,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract TokenDeposit is Ownable {
 
+    struct User {
+        address walletAddress;
+        Deposit deposit;
+    }
+
+    struct Deposit {
+        uint256 amount;
+        uint256 depositedTime;
+    }
+
     address public token;
+
+    mapping (address=>User) public user;
 
     constructor(address _tokenAddress)Ownable(msg.sender) {
         token = _tokenAddress;
@@ -14,6 +26,9 @@ contract TokenDeposit is Ownable {
 
     function deposit(uint256 amount) external returns(bool){
         IERC20(token).transferFrom(msg.sender,address(this),amount);
+        User memory u = user[msg.sender];
+        u.deposit.amount = amount;
+        u.deposit.depositedTime = block.timestamp;
         return true;
     }
 
